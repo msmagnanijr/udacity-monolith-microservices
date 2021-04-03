37338e47b8a91129f460871635f97333d4f5146b
Mauricio Magnani <msmagnanijr@gmail.com>
2021-04-03 13:01:06 -0300

    add cm file and export script

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=udagram
+export POSTGRES_PASSWORD=udagram2021
+export POSTGRES_HOST=udagram-instance-1.example.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=udagram
+export AWS_BUCKET=udagram-myaccount-final
+export AWS_REGION=us-east-1
+export AWS_PROFILE=udagram-dev
+export JWT_SECRET=helloworld
+export URL=http://localhost:8100
